41898ea3f9151f100db91142ab8e4ce8f8bcca10
Kelvin Bush <kelybush@gmail.com>
2022-04-04 16:51:06 +0100

    Set up and fix linter errors
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,23 +1,24 @@
-const navMenu = document.getElementById('nav-menu'), navToggle = document.getElementById('nav_toggle'),
-  navClose = document.getElementById('close-nav')
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav_toggle');
+const navClose = document.getElementById('close-nav');
 
 if (navToggle) {
   navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show__nav')
-  })
+    navMenu.classList.add('show__nav');
+  });
 }
 
 if (navClose) {
   navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show__nav')
-  })
+    navMenu.classList.remove('show__nav');
+  });
 }
 
-const navLink = document.querySelectorAll('.nav_link')
+const navLink = document.querySelectorAll('.nav_link');
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show__nav')
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show__nav');
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((link) => link.addEventListener('click', linkAction));
